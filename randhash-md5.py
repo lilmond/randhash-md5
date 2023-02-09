1d2c2a046e88b170d9169c95b60681be7783f915
lilmond <54730301+lilmond@users.noreply.github.com>
2023-02-09 12:50:00 +0800

    Update randhash-md5.py

--- randhash-md5.py
+++ randhash-md5.py
@@ -1,10 +1,20 @@
 import hashlib
 import random
 import string
+import base64
 import time
 
 def main():
     try:
+        print(f"\nSelect a generator type:\n\n:1 - Easy-Fast\n:2 - Hard-Slow\n:3 - Base64_Enc-Hardest-Slowest\n:4 - Base64_Enc-Easy-Fast\n")
+        try:
+            gen_types = [1, 2, 3, 4]
+            gen_type = int(input("Generator Type: "))
+            if not gen_type in gen_types: raise ValueError
+        except ValueError:
+            print("Error: Invalid generator type.")
+            return
+
         try:
             count = int(input("Number to generate: "))
         except ValueError:
@@ -14,8 +24,18 @@ def main():
         print("Generating random hash(es)...")
         hashes = []
         start_time = time.time()
-        for i in range(count):
-            hashes.append(hashlib.md5("".join(random.choices(string.printable, k=random.randrange(100, 999))).encode()).hexdigest())
+        if gen_type == 1:
+            for _ in range(count):
+                hashes.append(hashlib.md5("".join(random.choices(string.printable, k=20)).encode()).hexdigest())
+        elif gen_type == 2:
+            for _ in range(count):
+                hashes.append(hashlib.md5("".join(random.choices(string.printable, k=random.randrange(100, 999))).encode()).hexdigest())
+        elif gen_type == 3:
+            for _ in range(count):
+                hashes.append(hashlib.md5(base64.b64encode("".join(random.choices(string.printable, k=999)).encode())).hexdigest())
+        elif gen_type == 4:
+            for _ in range(count):
+                hashes.append(hashlib.md5(base64.b64encode("".join(random.choices(string.printable, k=20)).encode())).hexdigest())
         end_time = time.time()
 
         print("\n\nRandom Hashes:\n")
